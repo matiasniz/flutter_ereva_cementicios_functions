3b32f0a3452f367750a321ab07c8676174b09567
Matias <niz.matias@gmail.com>
2020-05-22 15:52:12 -0300

    pedidos function

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 const db = admin.firestore();
+
 exports.movCaja = functions.firestore
   .document("/empresas/{empresaId}/mov_cajas/{id}")
   .onCreate((snapshot, context) => {
@@ -29,4 +30,94 @@ exports.movCaja = functions.firestore
       .catch((err) => {
         console.log("Transaction failure:", err);
       }));
+  });
+
+exports.pedidos = functions.firestore
+  .document("/empresas/{empresaId}/pedidos/{id}")
+  .onWrite(async (change, context) => {
+    const empresaId = context.params.empresaId;
+
+    // Get an object with the current document value.
+    // If the document does not exist, it has been deleted.
+    const document = change.after.exists ? change.after.data() : null;
+
+    // Get an object with the previous document value (for update or delete)
+    const oldDocument = change.before.data();
+
+    if (document) {
+      if (oldDocument) {
+        console.log("se esta queriendo actualizar un pedido");
+        await Promise.all(
+          oldDocument.productos.map((p) => {
+            if (!p.color) return;
+
+            let productRef = db
+              .collection("empresas")
+              .doc(empresaId)
+              .collection("stock_productos")
+              .doc(p.producto + p.color);
+
+            return (transaction = db
+              .runTransaction((t) => {
+                return t.get(productRef).then((doc) => {
+                  let newSaldo = 0;
+                  if (doc && doc.data()) {
+                    newSaldo =
+                      (doc.data().demanda ? doc.data().demanda : 0) -
+                      p.cantidad;
+                  } else {
+                    newSaldo = p.cantidad;
+                  }
+                  t.update(productRef, { demanda: newSaldo });
+                });
+              })
+              .then((result) => {
+                console.log("Transaction success", result);
+              })
+              .catch((err) => {
+                console.log("Transaction failure:", err);
+              }));
+          })
+        );
+      } else {
+        console.log("se creo un nuevo pedido");
+      }
+      return Promise.all(
+        document.productos.map(async (p) => {
+          if (!p.color) return;
+
+          let productRef = db
+            .collection("empresas")
+            .doc(empresaId)
+            .collection("stock_productos")
+            .doc(p.producto + p.color);
+
+          let prod = await productRef.get();
+          if (prod && prod.data()) {
+            return (transaction = db
+              .runTransaction((t) => {
+                return t.get(productRef).then((doc) => {
+                  console.log("el articulo existe, actualizando demanda...");
+                  let newSaldo =
+                    (doc.data().demanda ? doc.data().demanda : 0) + p.cantidad;
+                  t.update(productRef, { demanda: newSaldo });
+                });
+              })
+              .then((result) => {
+                console.log("Transaction success", result);
+              })
+              .catch((err) => {
+                console.log("Transaction failure:", err);
+              }));
+          } else {
+            return productRef.set({
+              demanda: p.cantidad,
+              producto: p.producto,
+              color: p.color,
+              stock: 0,
+            });
+          }
+        })
+      );
+    }
   });
